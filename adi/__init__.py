b99aac6922744915dd463092ce4ef6efc2ad3028
Andrew McCann <andrew.b.mccann@gmail.com>
2024-08-25 10:49:13 -0400

    updated __init__.py to include adf4351.py

--- adi/__init__.py
+++ adi/__init__.py
@@ -53,6 +53,7 @@ from adi.ada4961 import ada4961
 from adi.adaq8092 import adaq8092
 from adi.adar1000 import adar1000, adar1000_array
 from adi.adf4159 import adf4159
+from adi.adf4351 import adf4351
 from adi.adf4355 import adf4355
 from adi.adf4371 import adf4371
 from adi.adf5610 import adf5610
